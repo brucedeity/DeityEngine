485a4dbc4ab4e65f681f60f3b797e6c0501d58c4
brucedeity <ilaharl.bpf@gmail.com>
2023-05-24 22:52:03 -0300

    Created the window.

--- DeityEngine/Source/WinMain.cpp
+++ DeityEngine/Source/WinMain.cpp
@@ -1,7 +1,75 @@
+#include <windows.h>
 
-#include <iostream>
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
 
-int main()
+WCHAR	WindowClass[MAX_NAME_STRING];
+WCHAR	WindowTitle[MAX_NAME_STRING];
+
+INT		WindowWidth;
+INT		WindowHeight;
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT)
 {
+	wcscpy_s(WindowClass, TEXT("WindowClass"));
+	wcscpy_s(WindowTitle, TEXT("Deity's Game Engine"));
+	WindowWidth = 1366;
+	WindowHeight = 768;
 
+
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+
+	wcex.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wcex.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	wcex.lpszClassName = WindowClass;
+	wcex.lpszMenuName = nullptr;
+
+	wcex.hInstance = HInstance();
+
+	wcex.lpfnWndProc = DefWindowProc;
+
+	RegisterClassEx(&wcex);
+
+	HWND hWnd = CreateWindowW(
+		WindowClass,
+		WindowTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		WindowWidth,
+		WindowHeight,
+		nullptr,
+		nullptr,
+		HInstance(),
+		nullptr
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(0, L"Could't create the window!", 0, 0);
+		return 0;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
 }
